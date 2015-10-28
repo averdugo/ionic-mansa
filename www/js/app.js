eaa8b79841dc61236b627278726a2bfee6076f44
Aldo Merki <you@example.com>
2015-10-27 23:19:12 -0300

    removing bindonce

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@ const Server="http://skizzordz.exis.cl:8000/";
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires',[]
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers','ngCordova', 'ngOpenFB','ja.qr', 'pasvaz.bindonce'])
+angular.module('starter', ['ionic', 'starter.controllers','ngCordova', 'ngOpenFB','ja.qr'])
 
     .run(function($ionicPlatform, ngFB) {
       ngFB.init({appId: '869739519812527'});
